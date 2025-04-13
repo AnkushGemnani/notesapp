69d55b306d06ae5433017e2c2b80a4d56153daba
Ankush Gemnani <amazingankush2714.gmail.com>
2025-04-14 01:48:17 +0530

    Add deployment configuration for Render

---
+++ server.js
@@ -0,0 +1,5 @@
+// This file is used by Render to start the server
+// It simply requires the actual server implementation from the server directory
+
+console.log('Starting Notes application server...');
+require('./server/server'); 
